f2e9ab3d62c3a2029556f7aa30c9302c206bd564
shashank-github-0001 <shashankbharadwaj0001@gmail.com>
2024-01-05 21:32:02 +0530

    Fri Jan  5 09:32:02 PM IST 2024

---
+++ nvim/lua/shashank/plugins.lua
@@ -0,0 +1,138 @@
+local plugins = {
+    ---------------------------------------------------------------
+    { "folke/neoconf.nvim", cmd = "Neoconf" },
+    ---------------------------------------------------------------
+    "folke/neodev.nvim",
+    ---------------------------------------------------------------
+    --commenting
+    {
+        "numToStr/Comment.nvim",
+        config = function()
+            require("Comment").setup {}
+        end
+    },
+    ---------------------------------------------------------------
+    --lualine
+    {
+        'nvim-lualine/lualine.nvim',
+        dependencies = { 'nvim-tree/nvim-web-devicons' },
+        config = function()
+            require("lualine").setup {
+                icons_enabled = true,
+                theme = 'tokyonight'
+            }
+        end
+    },
+    ---------------------------------------------------------------
+    --treeshitter
+    {
+        'nvim-treesitter/nvim-treesitter',
+        build = ':TSUpdate',
+    },
+    ---------------------------------------------------------------
+    --telescope
+    {
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.4',
+        dependencies = { 'nvim-lua/plenary.nvim' },
+    },
+    ---------------------------------------------------------------
+    --tmux-nvim
+    'christoomey/vim-tmux-navigator',
+    ---------------------------------------------------------------
+    --surround
+    {
+        "kylechui/nvim-surround",
+        version = "*",
+        event = "VeryLazy",
+        config = function()
+            require("nvim-surround").setup({
+            })
+        end
+    },
+    ---------------------------------------------------------------
+    --autopairs
+    {
+        'windwp/nvim-autopairs',
+        event = "InsertEnter",
+        opts = {}
+    },
+    ---------------------------------------------------------------
+    --colorscheme
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = {},
+        config = function()
+            vim.cmd("colorscheme tokyonight-night")
+        end
+    },
+    ---------------------------------------------------------------
+    --codeium
+    "Exafunction/codeium.vim",
+    ---------------------------------------------------------------
+    --nvim-treesitter-playground
+    "nvim-treesitter/playground",
+    ---------------------------------------------------------------
+    --undo tree
+    "mbbill/undotree",
+    ---------------------------------------------------------------
+    --for git
+    "tpope/vim-fugitive",
+    ---------------------------------------------------------------
+    --for i dont know why
+    "nvim-tree/nvim-web-devicons",
+    ---------------------------------------------------------------
+    --for lsp keymaps and stuff
+    "neovim/nvim-lspconfig",
+    ---------------------------------------------------------------
+    --for downloading language servers
+    "williamboman/mason.nvim",
+    ---------------------------------------------------------------
+    --for mason-lspconfig i dont know why
+    "williamboman/mason-lspconfig.nvim",
+    ---------------------------------------------------------------
+    'christoomey/vim-tmux-navigator',
+    ---------------------------------------------------------------
+    {
+        "L3MON4D3/LuaSnip",
+        version = "v2.*",
+        build = "make install_jsregexp"
+    },
+    ---------------------------------------------------------------
+    {
+        'hrsh7th/nvim-cmp',
+        dependencies = {
+            'hrsh7th/cmp-nvim-lsp',
+            'hrsh7th/cmp-buffer',
+            'hrsh7th/cmp-path',
+            'hrsh7th/cmp-cmdline',
+            'hrsh7th/nvim-cmp',
+            'L3MON4D3/LuaSnip',
+            'saadparwaiz1/cmp_luasnip',
+            'rafamadriz/friendly-snippets',
+            'hrsh7th/cmp-nvim-lsp'
+        }
+    },
+    ---------------------------------------------------------------
+    { "vimwiki/vimwiki" },
+    ---------------------------------------------------------------
+}
+
+
+
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
+end
+vim.opt.rtp:prepend(lazypath)
+require("lazy").setup(plugins)
