aec195dc2c26143f097a11519a7ad923e30a262e
shashank-github-0001 <shashankbharadwaj0001@gmail.com>
2024-01-07 16:17:49 +0530

    Sun Jan  7 04:17:49 PM IST 2024

--- nvim/lua/shashank/viki.lua
+++ nvim/lua/shashank/viki.lua
@@ -1,6 +1,6 @@
-vim.g.vimwiki_list = {{
-  path = "~/vimwiki/",
-  syntax = "markdown",
-  ext = ".md"
-}}
+vim.g.vimwiki_list = { {
+    syntax = "markdown",
+    ext = ".md"
+} }
 
+vim.g.vimwiki_global_ext = 0
