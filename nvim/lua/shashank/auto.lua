02a79628c3db92e7b463f7c9a1a7e175176d8979
shashank-github-0001 <shashankbharadwaj0001@gmail.com>
2024-01-09 22:08:14 +0530

    Tue Jan  9 10:08:14 PM IST 2024

--- nvim/lua/shashank/auto.lua
+++
@@ -1,3 +0,0 @@
--- vim.keymap.set('i', '<c-j>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true, silent = true })
--- vim.keymap.set('i', '<c-k>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true, silent = true })
--- vim.keymap.set('i', '<Tab>', function () return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
