76b54130f9c9b781af58c253d3481cefb7c78809
shashank-github-0001 <shashankbharadwaj0001@gmail.com>
2024-01-09 22:21:14 +0530

    Tue Jan  9 10:21:14 PM IST 2024

--- nvim/lua/shashank/plugins.lua
+++ nvim/lua/shashank/plugins.lua
@@ -117,6 +117,17 @@ local plugins = {
     --vimwiki
     { "vimwiki/vimwiki" },
     ---------------------------------------------------------------
+    --note taking
+    {
+    "nvim-neorg/neorg",
+    run = ":Neorg sync-parsers", -- This is the important bit!
+    config = function()
+        require("neorg").setup {
+            -- configuration here
+        }
+    end,
+}
+    ---------------------------------------------------------------
 }
 
 
